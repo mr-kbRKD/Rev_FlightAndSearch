a553376cbab2cf935a60f435e848b4ad84e5e18b
KULBHUSHAN <krishnathakur0007895@gmail.com>
2023-03-17 18:34:52 +0530

    Added routers to complete create api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig')
-const { CityRepository } = require('./repository/city-repository');
+const  ApiRoutes = require('./routes/index')
+
+// const { CityRepository } = require('./repository/city-repository');
+
 const setupAndStartServer = async () =>{
 
     const app = express();
@@ -10,9 +13,11 @@ const setupAndStartServer = async () =>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended : true}));
     
+    app.use('/api', ApiRoutes);
+
     app.listen(PORT, ()=>{
         console.log(`server started at ${PORT}`);
-        const city = new CityRepository();
+        // const city = new CityRepository();
     }); 
 }  
 
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,6 +1,7 @@
 const {City} = require('../models/index');
 
 class CityRepository{
+    
     async createCity({ name }){
         try {
             const city = await City.create({
